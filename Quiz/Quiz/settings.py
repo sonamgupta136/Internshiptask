a6dde4680b94d0bf43ed122186c1617cb0add8f5
sonam gupta <66966120+sonamgupta136@users.noreply.github.com>
2021-06-08 04:59:37 -0700

    commit

--- Quiz/Quiz/settings.py
+++ Quiz/Quiz/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-19s*6k)l&9xg-7s-uh=o85)v5$5bjmlvcs!-3h8#w$=iz1c^sh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- Quiz/Quiz/urls.py
+++ Quiz/Quiz/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
+    
+from django.views.static import serve
 
 
 urlpatterns = [
@@ -24,7 +27,9 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
     path('',include('quizes.urls', namespace='quizes')),
-    
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
+
 
 urlpatterns += static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
